95058ede6a49fd8300b33bfced6caf7e46044d76
PurushottamHari <52137793+PurushottamHari@users.noreply.github.com>
2019-12-06 17:32:12 +0530

    Add files via upload
    The first lab session in class

---
+++ nlp1.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  6 16:14:44 2019
+
+@author: PURUSHOTTAM
+"""
+
+text = "Shakespeare gave the world best of theatrics"
+words = text.split(" ")
+print(words[0])
+print(text)
+
+text1 = '''William Shakespeare (bapt. 26 April 1564 – 23 April 1616)[a] was an English poet
+, playwright, and actor, widely regarded as the greatest writer in the English language and 
+the world's greatest dramatist.[2][3][4] He is often called England's national poet and the 
+"Bard of Avon" (or simply "the Bard").[5][b] His extant works, including collaborations, consist of some 39 plays,[c] 154 sonnets, two long narrative poems, and a few other verses, some of uncertain authorship. His plays have been translated into every major living language and are performed more often than those of any other playwright.[7]'''
+print(text1)
+
+text3 = 'Shakespeare didn\'t give the world anything'
+
+text4 = 'संत कबीर दास के दोहे गागर में सागर के समान हैं। उनका गूढ़ अर्थ समझ कर यदि कोई उन्हें अपने जीवन में उतारता है तो उसे निश्चय ही मन की शांति के साथ-साथ ईश्वर की प्राप्ति होगी।'
+print(text4)
+
